9cfd4ce8091deb22b90c0992482ed68816d8b270
RSILVYLELIGOIS <romain.silvyleligois@ynov.com>
2021-05-10 17:08:23 +0200

    timer

--- data/script.js
+++ data/script.js
@@ -2,15 +2,22 @@ let idRect = 1
 let interval
 let t = 200
 let point = 0
+let timer;
+let seconds = 6
 
 const start = () => {
   document.querySelector('#start').addEventListener('click', ()=>{
     createBrick()
-    timer()
+    launchTimer()
     gameLoop()
   })
 }
 
+const gameLoop = () => {
+  moveRect()
+  checkPoint()
+  requestAnimationFrame(gameLoop);
+}
 
 const moveRect = () => {
   let rect = document.querySelectorAll(".rect")
@@ -32,29 +39,46 @@ const checkPoint = () => {
 }
 
 const createBrick = () => {
-    clearInterval(interval);
-    let newdiv = document.createElement("div");
-    newdiv.setAttribute("class", 'tile' + String(Math.floor(Math.random() * (5 - 1 + 1)) + 1))
-    newdiv.className += ' rect'
-    newdiv.style.top = '-110px'
-    newdiv.style.height = Math.floor(Math.random() * (100 - 50 + 1)) + 50 + "px"
-    newdiv.id = 'rect-' + idRect
-    newdiv.addEventListener("click", () => {
-      playSound(newdiv.classList)
-      newdiv.remove()
-      point++
-      document.getElementById('affichePoint').innerHTML = point
-    })
+  clearInterval(interval);
+  let newdiv = document.createElement("div");
+  newdiv.setAttribute("class", 'tile' + String(Math.floor(Math.random() * (5 - 1 + 1)) + 1))
+  newdiv.className += ' rect'
+  newdiv.style.top = '-110px'
+  newdiv.style.height = Math.floor(Math.random() * (100 - 50 + 1)) + 50 + "px"
+  newdiv.id = 'rect-' + idRect
+  newdiv.addEventListener("click", () => {
+    playSound(newdiv.classList)
+    newdiv.remove()
+    point++
+    document.getElementById('affichePoint').innerHTML = point
+  })
 
-    document.getElementById('piano').append(newdiv)
-    idRect +=1
-    changeTimer();
-    interval = setInterval(createBrick, t);
-  }
+  document.getElementById('piano').append(newdiv)
+  idRect +=1
+  changeTimer();
+  interval = setInterval(createBrick, t);
+}
 
-  function changeTimer(){
-    t = Math.floor(Math.random() * (1000 - 500 + 1)) + 500
-  }
+function changeTimer(){
+  t = Math.floor(Math.random() * (1000 - 500 + 1)) + 500
+}
+
+const launchTimer = () => {
+  timer = window.setInterval(function() {
+    if (seconds >0 ) {
+      seconds--;
+      document.querySelector("#countDown").innerHTML = seconds
+    } else {
+      clearInterval(timer);
+      clearInterval(interval)
+      for (let rect of document.querySelectorAll(".rect")) {
+        rect.remove()
+      }
+    }
+  }, 1000);
+}
+
+
 
 
 const playSound = (classList) => {
