e5d6951181e5df9289f6d4e334ac94067c3bac6a
RSILVYLELIGOIS <romain.silvyleligois@ynov.com>
2021-05-12 10:54:04 +0200

    test remove file

--- data/script.js
+++ data/script.js
@@ -8,8 +8,28 @@ let timer;
 let animation
 let runing = false
 
+
 //launched at start 
 const start = () => {
+  displayHistory()
+  checkStart()
+}
+
+
+//take the localstorage var named history and display it in a table
+const displayHistory = () => {
+  let history = localStorage.getItem('history')
+  if (history != null) {
+    history = history.split('|')
+    for (let i=0; i < history.length; i += 2) {
+      createRow(history[i], history[i+1])
+    }
+  }
+}
+
+
+//launch the gameloop if startButton clicked
+const checkStart = () => {
   document.querySelector('#start').addEventListener('click', ()=>{
     if (!runing) {
       runing = true
@@ -20,26 +40,124 @@ const start = () => {
   })
 }
 
-const displayHistory = () => {
+
+//create random height bricks at a random interval 
+const createBrick = () => {
+  clearInterval(interval);
+
+  let newdiv = document.createElement("div");
+  newdiv.setAttribute("class", 'tile' + String(randomizer(1, 5)))
+  newdiv.className += ' rect'
+  newdiv.style.top = '-110px'
+  newdiv.style.height = randomizer(50, 150) + "px"
+  newdiv.id = 'rect-' + idRect
+  document.getElementById('piano').append(newdiv)
+
+  newdiv.addEventListener("click", () => {
+    playSound(newdiv.classList)
+    newdiv.remove()
+    point++
+    document.getElementById('affichePoint').innerHTML = point
+  })
+
+  idRect +=1
+  t = randomizer(500, 1500)
+  interval = setInterval(createBrick, t);
+}
+
+//return random number between 2 numbers passed as parameter
+const randomizer = (low, high) => {
+  return Math.floor(Math.random() * (high - low + 1)) + low
+}
+
+//play sound when tile clicked
+const playSound = (classList) => {
+  if (classList.contains("tile1")) {
+    new Audio('data/mouton.mp3').play();
+  } else if (classList.contains("tile2")) {
+    new Audio('data/chevre.mp3').play();
+  } else if (classList.contains("tile3")) {
+    new Audio('data/cheval.mp3').play();
+  } else if (classList.contains("tile4")) {
+    new Audio('data/sophie.mp3').play();
+  } else if (classList.contains("tile5")) {
+    new Audio('data/cochon.mp3').play();
+  }
+}
+
+
+//launch a timer of 60s
+const launchTimer = () => {
+  timer = window.setInterval(function() {
+    if (seconds >0 ) {
+      seconds--;
+      document.querySelector("#countDown").innerHTML = seconds + "s"
+    } else {
+      pauseAndRemoveAll()
+      addHistory()  
+      displayLastHistory()    
+    }
+  }, 1000);
+}
+
+//pause the animationframe and remove the rect on the screen 
+const pauseAndRemoveAll = () => {
+  pause() 
+  for (let rect of document.querySelectorAll(".rect")) {
+    rect.remove()
+  }
+}
+
+//pause everything
+const pause = () => {
+  cancelAnimationFrame(animation);
+  clearInterval(interval)
+  clearInterval(timer)
+}
+
+//add score to history 
+const addHistory = () => {
+  let history = localStorage.getItem('history')
+  if (history === null) {
+    let toAdd = getDate() + "|" + point
+    localStorage.setItem('history', toAdd)
+  } else {
+    let toAdd = getDate() + "|" + point
+    localStorage.setItem('history', history.concat('|', toAdd))
+  }
+}
+
+//return date and time
+const getDate = () => {
+  let date = new Date();
+  let time = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate() + ":" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+  return time
+}
+
+//display the last row of the history when the game is finished
+const displayLastHistory = () => {
   let history = localStorage.getItem('history')
   if (history != null) {
     history = history.split('|')
-    for (let i=0; i < history.length; i += 2) {
-      let newTr = document.createElement('tr')
-      let newTdDate = document.createElement('td')
-      let newTdScore = document.createElement('td')
-      newTdDate.textContent = history[i]
-      newTdScore.textContent = history[i+1]
-      newTr.appendChild(newTdDate)
-      newTr.appendChild(newTdScore)
-      document.querySelector('table').append(newTr)
-    }
+    createRow(history[history.length-2], history[history.length-1])
   }
+}
+
+//create a row in the table for the displayHistory function
+const createRow = (date, score) => {
+  let newTr = document.createElement('tr')
+  let newTdDate = document.createElement('td')
+  let newTdScore = document.createElement('td')
+  newTdDate.textContent = date
+  newTdScore.textContent = score
+  newTr.appendChild(newTdDate)
+  newTr.appendChild(newTdScore)
+  document.querySelector('table').append(newTr)
 }
 
 //launched when start button clicked
 const gameLoop = () => {
-  moveRect(3)
+  moveRect(4)
   checkPause()
   animation = requestAnimationFrame(gameLoop);
   checkPoint()
@@ -79,93 +197,4 @@ const checkPoint = () => {
   }
 }
 
-//create random height bricks at a random interval 
-const createBrick = () => {
-  clearInterval(interval);
-
-  let newdiv = document.createElement("div");
-  newdiv.setAttribute("class", 'tile' + String(randomizer(1, 5)))
-  newdiv.className += ' rect'
-  newdiv.style.top = '-110px'
-  newdiv.style.height = randomizer(50, 150) + "px"
-  newdiv.id = 'rect-' + idRect
-  newdiv.addEventListener("click", () => {
-    playSound(newdiv.classList)
-    newdiv.remove()
-    point++
-    document.getElementById('affichePoint').innerHTML = point
-  })
-
-  document.getElementById('piano').append(newdiv)
-  idRect +=1
-  t = randomizer(500, 1500)
-  interval = setInterval(createBrick, t);
-}
-
-//return random number between 2 numbers passed as parameter
-const randomizer = (low, high) => {
-  return Math.floor(Math.random() * (high - low + 1)) + low
-}
-
-//launch a timer of 60s
-const launchTimer = () => {
-  timer = window.setInterval(function() {
-    if (seconds >0 ) {
-      seconds--;
-      document.querySelector("#countDown").innerHTML = seconds + "s"
-    } else {
-      pauseAndRemoveAll()
-      addHistory()      
-    }
-  }, 1000);
-}
-
-//pause everything
-const pause = () => {
-  cancelAnimationFrame(animation);
-  clearInterval(interval)
-  clearInterval(timer)
-}
-
-//pause the animationframe and remove the rect on the screen 
-const pauseAndRemoveAll = () => {
-  pause() 
-  for (let rect of document.querySelectorAll(".rect")) {
-    rect.remove()
-  }
-}
-
-
-const playSound = (classList) => {
-  if (classList.contains("tile1")) {
-    new Audio('data/mouton.mp3').play();
-  } else if (classList.contains("tile2")) {
-    new Audio('data/chevre.mp3').play();
-  } else if (classList.contains("tile3")) {
-    new Audio('data/cheval.mp3').play();
-  } else if (classList.contains("tile4")) {
-    new Audio('data/sophie.mp3').play();
-  } else if (classList.contains("tile5")) {
-    new Audio('data/cochon.mp3').play();
-  }
-}
-
-const getDate = () => {
-  let date = new Date();
-  let time = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate() + ":" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
-  return time
-}
-
-const addHistory = () => {
-  let history = localStorage.getItem('history')
-  if (history === null) {
-    let toAdd = getDate() + "|" + point
-    localStorage.setItem('history', toAdd)
-  } else {
-    let toAdd = getDate() + "|" + point
-    localStorage.setItem('history', history.concat('|', toAdd))
-  }
-}
-
-displayHistory()
 start()
